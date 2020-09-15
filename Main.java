6e6ebde5aecf554e1e0f453d843db685fa91a24d
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-15 12:27:54 +0000

    made it tell you what int is even or odd

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
-/**
- *
- * @author 
- */
+import java.util.Scanner;
+
 public class Main {
 
   /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
+   * this code will tell you if the number you inputed is even or odd
+   * @author Isaac
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+     System.out.println("Please enter an integer.");
+     int number = input.nextInt();
+     int numberRemainder = number % 2;
+     if (numberRemainder ==1) { 
+       System.out.println (+ number + " is an odd number." );
+       }  if (numberRemainder ==0) {
+         System.out.println (+ number + " is an even number.");
+       }
+     }
   }
-}
+       
+
